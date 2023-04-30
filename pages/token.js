516135e2b7befc94452db47aaa92984eb012c292
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-04-30 22:54:04 +0100

    commit

--- pages/token.js
+++ pages/token.js
@@ -14,25 +14,34 @@ const Token = () => {
 
   
   return (
-    <div>
+    <div className='relative h-screen w-full'>
       <h2>Get Our Token</h2>
-      <div className='flex flex-row items-center'>
+      <div className='flex flex-row items-center h-full'>
         <div className='flex flex-col flex-1 text-black'>
+        <div>
+        <Image
+        src="/undraw_crypto_flowers_re_dyqo.svg"
+        alt="Crypto flowers"
+        width={500}
+        height={700}
+      />
+        </div>
          <h2>Get our token in simple steps</h2>
          <p>Get NGN to insure yourself and stay protected.</p>
          <p>BOX</p>
         </div>
-        <div className='flex flex-col flex-1'>
+
+        <div className='flex flex-col flex-1 h-full'>
           <form>
 
             <div className=' flex flex-col'>
               <span className=' ml-10 font-semibold text-black
               text-xl'>Buy</span>
-              <div className='border-b-4 border-[#27AE60] w-1/5 mt-2'/>
+              <div className='border-b-4 border-[#19192E] w-1/5 mt-2'/>
             </div>
 
-            <div className='relative mt-12 w-full'>
-              <div className='bg-[#FAFAFA] w-4/5 mt-6 cursor-pointer p-4 rounded-3xl hover:border-2 border-[#27AE60] ease-in-out'>
+            <div className='relative mt-12 w-full h-full'>
+              <div className='bg-[#FAFAFA] w-4/5 h-fit mt-6 cursor-pointer p-4 rounded-3xl hover:border-[1px] border-[#19192E] ease-in-out duration-100'>
                 <div className='flex flex-col'>
                   <span className='text-black'>Spend</span>
                     <div className='flex flex-row items-center justify-between'>
@@ -71,7 +80,7 @@ const Token = () => {
                 </div>
               </div>
 
-              <div className='bg-[#FAFAFA] w-4/5 mt-6 cursor-pointer p-4 rounded-3xl hover:border-2 border-[#27AE60] ease-in-out '>
+              <div className='bg-[#FAFAFA] w-4/5 h-fit mt-6 cursor-pointer p-4 rounded-3xl hover:border-[1px] border-[#19192E] ease-in-out duration-100 '>
                 <div className='flex flex-col'>
                   <span className='text-black'>Receive</span>
                   <div className='flex flex-row items-center justify-between mt-4'>
@@ -93,7 +102,7 @@ const Token = () => {
                   </div>
                 </div>
               </div>
-              <button className='cursor-pointer w-2/5 bg-[#27AE60] text-white rounded-xl p-2 mt-9 hover:bg-[#FAFAFA] hover:text-black ease-in-out duration-300'>Buy</button>
+              <button className='cursor-pointer w-2/5 bg-[#19192E] text-white rounded-xl p-2 mt-9 hover:bg-[#FAFAFA] hover:text-black ease-in-out duration-300'>Buy</button>
             </div>
           </form>
         </div>
