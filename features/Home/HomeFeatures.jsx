36b21ef7613be5c4af0adf8ebd5c0481c47efd19
vicsnet <adesanmiadebisi@gmail.com>
2023-04-30 02:00:36 +0100

    features

---
+++ features/Home/HomeFeatures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeFeatures = () => {
+  return (
+    <main className=''>HomeFeatures</main>
+  )
+}
+
+export default HomeFeatures
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
 import TopBar from "@/components/TopBar";
+import HomeFeatures from "@/features/Home/HomeFeatures";
 
 
 export default function Home() {
   return (
     <main className='relative font-semibold'>
 <TopBar title="HOME"/>
+<div className="w-[96%] mx-auto">
 
-      Welcome
+<HomeFeatures/>
+</div>
     </main>
   )
 }
