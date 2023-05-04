76d84c16cad2f0c9e627b175468c34538dcd0caa
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-05-05 00:06:22 +0100

    added docs

--- pages/invest.js
+++ pages/invest.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Invest = () => {
   return (
-    <div>
-      
+    <div className=" flex items-center justify-center w-full h-full">
+      <div className="text-[#1A1941] font-bold text-xl">Coming Soon!</div>
     </div>
-  )
-}
+  );
+};
 
-export default Invest
+export default Invest;
